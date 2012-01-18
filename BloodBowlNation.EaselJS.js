64e85160392e63e390bec239620234c619992463
philpill <github@philpill.net>
2012-01-18 00:24:55 +0000

    pitch render

--- BloodBowlNation.EaselJS.js
+++ BloodBowlNation.EaselJS.js
@@ -17,52 +17,83 @@ var BBN = BBN || (function(){
 			canvasBounds.height = gameCanvas.height;
 			gameStage = new Stage(gameCanvas);
 			pitchStage = new Stage(pitchCanvas);
-
-			this.game.init(pitchStage, gameStage);
+			this.pitch.init(pitchStage, this.game);
+			this.game.init(gameStage);
 			Ticker.setFPS(30);
 			//Ticker.addListener(window);
 		},
-		game: {
+		pitch: {
+			gameContext: null,
+			unitBorderColour: null,
+			unitFillColour: null,
+			boundaryLineColour: null,
 			pitchStage: null,
-			gameStage: null,
-			init: function(pitchStage, gameStage) {
-				this.pitchStage = pitchStage;
-				this.gameStage = gameStage;
-				this.pitch.init(this.pitchStage, this.gameStage);
-			},
-			pitch: {
-				pitchStage: null,
-				gameStage: null,
-				gameContext: null,
-				backgroundImage: "Pitch.jpg",
-				render: function() {
-					var pitchImage = new Image();
-
-					pitchImage.src = this.backgroundImage;
-
-					pitchImage.onload = function() {
-					
-						var pitchBitmap = new Bitmap(pitchImage);	
-						
-						pitchStage.addChild(pitchBitmap);
-						
-						pitchBitmap.x = 0;
-						pitchBitmap.y = 0;
-						pitchBitmap.scaleX = 0.5;
-						pitchBitmap.scaleY = 0.5;
-		
-						pitchStage.update();
-					}						
-				},
-				init: function(pitchStage, gameStage) {
-					this.pitchStage = pitchStage;
-					this.gameStage = gameStage;	
-					this.render();
+			gameContext: null,
+			backgroundImage: "Pitch.jpg",
+			render: function() {
+				var gameContext = this.gameContext,
+					canvasContext = this.canvasContext,
+					unit = gameContext.pitchUnitSize,
+					width = gameContext.canvasWidth,
+					height = gameContext.canvasHeight,
+					unitBorderColour = this.unitBorderColour,
+					boundaryLineColour = this.boundaryLineColour,
+					pitchImage = new Image();
+				pitchImage.src = this.backgroundImage;
+				pitchImage.onload = function() {
+					var pitchBitmap = new Bitmap(pitchImage);
+					pitchStage.addChild(pitchBitmap);
+					pitchBitmap.x = 0;
+					pitchBitmap.y = 0;
+					pitchBitmap.scaleX = 0.435;
+					pitchBitmap.scaleY = 0.435;
+					var shape = new Shape();
+					pitchStage.addChild(shape);
+					//vertical grid lines
+					for (var x=0.5; x < (width*unit)+unit; x+=unit) {	
+						shape.graphics.beginStroke(boundaryLineColour).moveTo(x,0).lineTo(x,height*unit).endStroke();
+					}
+					//horizontal grid lines
+					for (var y=0.5; y < (height*unit)+unit; y+=unit) {
+						shape.graphics.beginStroke(boundaryLineColour).moveTo(0,y).lineTo(width*unit,y).endStroke();
+					}
+					pitchStage.update();
 				}
+			},
+			init: function(pitchStage, gameContext) {
+				this.gameContext=gameContext;
+				this.unitFillColour="rgba(0,255,0,0)";
+				this.unitBorderColour="rgba(0,0,0,0.1)";
+				this.boundaryLineColour="rgba(255,255,255,0.2)";
+				this.pitchStage = pitchStage;
+				this.render();
+			}
+		},
+		game: {
+			pitchUnitSize: 20,
+			canvasHeight: 26,
+			canvasWidth: 15,
+			gameStage: null,
+			grid: {
+				width: this.canvasWidth,					
+				length: this.canvasHeight,
+				unit: this.pitchUnitSize,
+				space: null				
+			},
+			init: function(gameStage) {
+				var i, j;
+			
+				this.grid.space = new Array(this.canvasWidth);
+				for (i = 0; i < this.canvasWidth; i++) {
+					this.grid.space[i] = new Array(this.canvasHeight);
+					for (j = 0; j < this.grid.space[i].length; j++) {
+						this.grid.space[i][j] = [];
+					}
+				}
+				this.gameStage = gameStage;
 			}		
 		}
 	}
-
 })();
 
 function tick() {
