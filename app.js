9e97913d09c574af3c5988525b11c626ae37ffb7
philpill <github@philpill.net>
2012-09-17 00:01:58 +0100

    space tidyup

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
 
-/**
- * Module dependencies.
- */
+(function(req) {
 
-var routes = require('./routes');
+	var routes = req('./routes');
 
-var express = require('express');
+	var express = req('express');
 
-var app = express();
+	var app = express();
 
-var server = require('http').createServer(app);
+	var server = req('http').createServer(app);
 
-var io = require('socket.io').listen(server);
+	var io = req('socket.io').listen(server);
 
-server.listen(3000);
+	server.listen(3000);
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('view options', { layout: false });
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+	app.configure(function(){
+		app.set('views', __dirname + '/views');
+		app.set('view engine', 'jade');
+		app.set('view options', { layout: false });
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		app.use(app.router);
+		app.use(express.static(__dirname + '/public'));
+	});
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+	app.configure('development', function(){
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	});
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+	app.configure('production', function(){
+		app.use(express.errorHandler());
+	});
 
-app.get('/', routes.index);
+	app.get('/', routes.index);
 
-app.get('/test', routes.test);
+	app.get('/test', routes.test);
 
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+	io.sockets.on('connection', function (socket) {
+		socket.emit('news', { hello: 'world' });
+		socket.on('my other event', function (data) {
+		console.log(data);
+		});
+	});
+
+})(require);
--- public/js/BBN.js
+++ public/js/BBN.js
@@ -22,12 +22,12 @@ require.config({
 
 requirejs(requires, function(BBN) {
 
-var socket = io.connect('http://localhost');
-  socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
+	var socket = io.connect('http://localhost');
 
+	socket.on('news', function (data) {
+		console.log(data);
+		socket.emit('my other event', { my: 'data' });
+	});
 
 	BBN.init();	
 });
