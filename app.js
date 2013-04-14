8673a8189027a96da8a1d0cb57951050ad30a3f7
philpill <github@philpill.net>
2013-04-14 17:36:40 +0100

    join game

--- app.js
+++ app.js
@@ -63,11 +63,11 @@
 	app.get('/about', routes.page.about);
 	app.get('/test', routes.page.test);
 
-    app.get('/game', routes.game.getAll);
-    app.get('/game/new', passport.ensureAuthenticated, routes.game.createGet);
-    app.post('/game/new', passport.ensureAuthenticated, routes.game.createPost);
-    app.get('/game/:id', routes.game.get);
-    app.get('/game/:id/join', routes.game.join);
+	app.get('/game', routes.game.getAll);
+  app.get('/game/new', passport.ensureAuthenticated, routes.game.createGet);
+	app.post('/game/new', passport.ensureAuthenticated, routes.game.createPost);
+	app.get('/game/:id', routes.game.get);
+	app.post('/game/:id/join', passport.ensureAuthenticated, routes.game.join);
 
 	app.get('/team', routes.team.getAll);
 	app.get('/team/new', routes.team.createGet);
@@ -93,11 +93,11 @@
 
 	app.get('/admin', passport.ensureAuthenticated, routes.admin.index);
 
-    //should be in race.js?
+  //should be in race.js?
 	app.get('/admin/race', passport.ensureAuthenticated, routes.admin.races);
     
-    app.get('/admin/user', passport.ensureAuthenticated, routes.user.getAll);
-    app.post('/admin/user/new', passport.ensureAuthenticated, routes.user.createPost);
+  app.get('/admin/user', passport.ensureAuthenticated, routes.user.getAll);
+  app.post('/admin/user/new', passport.ensureAuthenticated, routes.user.createPost);
 
 	app.get('/api/game', passport.ensureAuthenticated, api.game.getAll);
 	app.get('/api/player', passport.ensureAuthenticated, api.player.index);
--- routes/game.js
+++ routes/game.js
@@ -7,104 +7,105 @@ var _ = require('underscore');
 
 exports.getAll = function(req, res) {
 	var user = req.user;
-    Game.find()
-    .populate('hostTeam')
-    .exec(function(err, games){ 
-		if (err) res.send(500, { error: err });
-        console.log(games);
-        res.render('games', {
-            title:'BloodBowlNation: Games',
-            user: user,
-            games: games
-        });
-    });
+	Game.find()
+	.populate('hostTeam clientTeam host client')
+	.exec(function(err, games){ 
+	if (err) res.send(500, { error: err });
+			console.log(games);
+			res.render('games', {
+					title:'BloodBowlNation: Games',
+					user: user,
+					games: games
+			});
+	});
 };
 
 exports.createGet = function(req, res) {
 	var user = req.user;
 	if (!user) {
-        res.redirect('/login');
-    } else {
-        User.findById(user._id)
-        .populate('teams')
-        .exec(function(err, user){
-            if (err) res.send(500, {error: err});
-            //filter teams not in game
-            res.render('newGame', {
-                title: 'BloodBowlNation: New Game',
-                user: user,
-                teams: user.teams   
-            });
-        });
-    }
+				res.redirect('/login');
+		} else {
+				User.findById(user._id)
+				.populate('teams')
+				.exec(function(err, user){
+						if (err) res.send(500, {error: err});
+						//filter teams not in game
+						res.render('newGame', {
+								title: 'BloodBowlNation: New Game',
+								user: user,
+								teams: user.teams		
+						});
+				});
+		}
 };
 
 exports.createPost = function(req, res) {
-    var user = req.user;
-    var name = req.body.gameName;
-    var teamId = req.body.team;
-    console.log('new game');
-    console.log(req.body);
-    if (!user) res.redirect('/login');
-    //check user against team
-    //check team is viable for new game
-    var game = new Game();
-    game.name = name;
-    game.host = user._id;
-    game.gameTurn = 0;
-    game.hostTeam = teamId;
-    game.createBy = user._id;
-    game.save(function(err){
-        if (err) res.send(500, {error: err});
-        res.redirect('/game');
-    });
+		var user = req.user;
+		var name = req.body.gameName;
+		var teamId = req.body.team;
+		console.log('new game');
+		console.log(req.body);
+		if (!user) res.redirect('/login');
+		//check user against team
+		//check team is viable for new game
+		var game = new Game();
+		game.name = name;
+		game.host = user._id;
+		game.gameTurn = 0;
+		game.hostTeam = teamId;
+		game.createBy = user._id;
+		game.save(function(err){
+				if (err) res.send(500, {error: err});
+				res.redirect('/game');
+		});
 }
 
 exports.get = function(req, res){
-    var user = req.user;
-    var gameId = req.params.id;
-    Game.findById(gameId)
-    .populate('clientTeam hostTeam host client')
-    .exec(function(err, game){
-        if (err) res.send(500, {error: err});
-        console.log(game);
-        res.render('game', {
-            user: user,
-            title: 'BloodBowlNation: Game',
-            game: game
-        });
-    });
+	var user = req.user;
+	var gameId = req.params.id;
+	Game.findById(gameId)
+	.populate('clientTeam hostTeam host client')
+	.exec(function(err, game){
+		if (err) res.send(500, {error: err});
+		User.findById(user.id)
+		.populate('teams')
+		.exec(function(err, user){
+			console.log(game);
+			console.log(user);
+			res.render('game', {
+				user: user,
+				title: 'BloodBowlNation: Game',
+				game: game
+			});
+		});
+	});
 }
 
 exports.join = function(req, res) {
-    var gameId = req.params.id;
-    var user = req.user;
-    var team = req.body.team;
-    console.log(user);
-
-    //SELECT TEAM!
-
-
-    Game.findById(gameId)
-    .exec(function(err, game){
-        if (err) res.send(500, {error: err});
-        User.findById(user._id)
-        .exec(function(err, user) {
-            if (err) res.send(500, {error: err});
-            console.log(user);
-            if (_.contains(user.teams, team)) {
-                game.client = user.id;
-                game.clientTeam = team.id;
-                game.editBy = user.id;
-                game.editDate = Date.now;
-                game.save(function(err){
-                    if (err) res.send(500, {error:err});
-                    res.redirect('/game');
-                }); 
-            }
-        });
-    });
-    res.send(500);
+	var gameId = req.params.id;
+	var user = req.user;
+	var team = req.body.team;
+	Game.findById(gameId)
+	.exec(function(err, game){
+		if (err) res.send(500, {error: err});
+		User.findById(user._id)
+		.exec(function(err, user) {
+			if (err) res.send(500, {error: err});
+			Team.findById(team)
+			.where('_id').in(user.teams)
+			.exec(function(err, team){
+				if (err) res.send(500, {error: err});
+				game.client = user.id;
+				game.clientTeam = team.id;
+				game.editBy = user.id;
+				game.editDate =  new Date();
+				game.save(function(err){
+					if (err) res.send(500, {error:err}); 
+					res.redirect('/game');
+				}); 
+			});
+		});
+	});
 }
 
 
