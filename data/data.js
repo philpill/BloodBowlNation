e3bed353d05dfa7ef981ec6145a1cf1de42358e1
philpill <github@philpill.net>
2016-04-06 21:37:11 +0100

    add insert queries

--- data/data.js
+++ data/data.js
@@ -4,45 +4,53 @@ var Promise = require('bluebird');
 
 var conn = 'postgres://philpill@localhost:5432/philpill';
 
-//*
-var createUserTable = 'CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, email TEXT NOT NULL, password TEXT NOT NULL);';
-var createTeamTable = 'CREATE TABLE IF NOT EXISTS teams (id SERIAL PRIMARY KEY, name TEXT NOT NULL, race SERIAL NOT NULL REFERENCES races(ID), managerId SERIAL REFERENCES users(ID));';
-var createRaceTable = 'CREATE TABLE IF NOT EXISTS races (id SERIAL PRIMARY KEY, name TEXT NOT NULL);';
-var createPositionTable = 'CREATE TABLE IF NOT EXISTS positions (id SERIAL PRIMARY KEY, name TEXT NOT NULL, race SERIAL NOT NULL REFERENCES races(ID));';
-var createPlayerTable = 'CREATE TABLE IF NOT EXISTS players (id SERIAL PRIMARY KEY, name TEXT NOT NULL, race SERIAL NOT NULL REFERENCES races(ID), positionId SERIAL NOT NULL REFERENCES positions(ID), teamId SERIAL REFERENCES teams(ID));';
-//*/
-/*
-var createUserTable = 'DROP TABLE IF EXISTS users CASCADE; CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, email TEXT NOT NULL, password TEXT NOT NULL);';
-var createTeamTable = 'DROP TABLE IF EXISTS teams CASCADE; CREATE TABLE IF NOT EXISTS teams (id SERIAL PRIMARY KEY, name TEXT NOT NULL, race SERIAL NOT NULL REFERENCES races(ID), managerId SERIAL REFERENCES users(ID));';
-var createRaceTable = 'DROP TABLE IF EXISTS races CASCADE; CREATE TABLE IF NOT EXISTS races (id SERIAL PRIMARY KEY, name TEXT NOT NULL);';
-var createPositionTable = 'DROP TABLE IF EXISTS positions CASCADE; CREATE TABLE IF NOT EXISTS positions (id SERIAL PRIMARY KEY, name TEXT NOT NULL, race SERIAL NOT NULL REFERENCES races(ID));';
-var createPlayerTable = 'DROP TABLE IF EXISTS players CASCADE; CREATE TABLE IF NOT EXISTS players (id SERIAL PRIMARY KEY, name TEXT NOT NULL, race SERIAL NOT NULL REFERENCES races(ID), positionId SERIAL NOT NULL REFERENCES positions(ID), teamId SERIAL REFERENCES teams(ID));';
-//*/
+var createUserTable = 'CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL);';
+var createTeamTable = 'CREATE TABLE IF NOT EXISTS teams (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE, race SERIAL NOT NULL REFERENCES races, managerId SERIAL REFERENCES users);';
+var createRaceTable = 'CREATE TABLE IF NOT EXISTS races (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE);';
+var createPositionTable = 'CREATE TABLE IF NOT EXISTS positions (id SERIAL PRIMARY KEY, name TEXT NOT NULL, race SERIAL NOT NULL REFERENCES races, movement SMALLINT NOT NULL, strength SMALLINT NOT NULL, agility SMALLINT NOT NULL, armour SMALLINT NOT NULL);';
+var createPlayerTable = 'CREATE TABLE IF NOT EXISTS players (id SERIAL PRIMARY KEY, name TEXT NOT NULL, race SERIAL NOT NULL REFERENCES races, positionId SERIAL NOT NULL REFERENCES positions(ID), teamId SERIAL REFERENCES teams);';
+
+var populateRacesTable = `INSERT INTO races (name) VALUES ('human'),('orc') ON CONFLICT DO NOTHING;`
+
+var populatePositionsTable = `
+    INSERT INTO positions (name, race, movement, strength, agility, armour) VALUES ('lineman', (SELECT id FROM races WHERE name = 'human'), 6, 3, 3, 8) ON CONFLICT DO NOTHING;
+    INSERT INTO positions (name, race, movement, strength, agility, armour) VALUES ('blitzer', (SELECT id FROM races WHERE name = 'human'), 7, 3, 3, 8) ON CONFLICT DO NOTHING;
+    INSERT INTO positions (name, race, movement, strength, agility, armour) VALUES ('thrower', (SELECT id FROM races WHERE name = 'human'), 6, 3, 3, 8) ON CONFLICT DO NOTHING;
+    INSERT INTO positions (name, race, movement, strength, agility, armour) VALUES ('catcher', (SELECT id FROM races WHERE name = 'human'), 8, 2, 3, 7) ON CONFLICT DO NOTHING;`;
 
 pg.connect(conn, function onConnect (err, client, done) {
 
     if (err) { console.log(err); }
-
+    //*
     client.query(createUserTable, function (err, result) {
-        done();
-        if (err) { console.log(err); }
-    });
-    client.query(createTeamTable, function (err, result) {
-        done();
         if (err) { console.log(err); }
     });
     client.query(createRaceTable, function (err, result) {
-        done();
         if (err) { console.log(err); }
+
+        client.query(populateRacesTable, function (err, result) {
+            if (err) { console.log(err); }
+        });
+
+        client.query(createTeamTable, function (err, result) {
+            if (err) { console.log(err); }
+        });
+        client.query(createPositionTable, function (err, result) {
+            if (err) { console.log(err); }
+
+            client.query(populatePositionsTable, function (err, result) {
+                if (err) { console.log(err); }
+            });
+
+            client.query(createPlayerTable, function (err, result) {
+                if (err) { console.log(err); }
+            });
+        });
     });
-    client.query(createPositionTable, function (err, result) {
-        done();
-        if (err) { console.log(err); }
-    });
-    client.query(createPlayerTable, function (err, result) {
-        done();
-        if (err) { console.log(err); }
-    });
+
+    done();
+
+    // drop schema public cascade; create schema public;
 });
 
 
